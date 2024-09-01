c930545a4bc4787a01855033b5b5b403196f0a2a
wawanharianto <wawanharianto696@gmail.com>
2024-09-01 14:37:43 +0800

    Backend CRUD applikasi note

---
+++ src/server.js
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes:{
+      cors:{
+        origin:['*'],
+        additionalHeaders: ['cache-control', 'x-requested-with'],
+      },
+    },
+  });
+
+  server.route(routes);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+
+init();
