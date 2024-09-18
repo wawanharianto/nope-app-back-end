cf1d4bf2126c72c74fe43c9a81dbdbeff4303a66
wawanharianto <wawanharianto696@gmail.com>
2024-09-18 12:49:54 +0800

    update ssl for public

--- src/server.js
+++ src/server.js
@@ -1,22 +1,46 @@
 /* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
+const https = require('https'); // Mengimpor modul https
+const fs = require('fs'); // Mengimpor modul fs untuk membaca file
 const routes = require('./routes');
 
+// Fungsi untuk menginisialisasi server
 const init = async () => {
+  // Membaca sertifikat dan kunci
+  const options = {
+    key: fs.readFileSync('./certs/server.key'), // Ganti dengan path ke file kunci
+    cert: fs.readFileSync('./certs/server.crt'), // Ganti dengan path ke file sertifikat
+  };
+
+  // Mengatur server dengan Hapi
   const server = Hapi.server({    
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-        cors: {
-            origin: ['*'],
-        },
+      cors: {
+        origin: ['*'], // Mengizinkan CORS dari semua origin
+      },
     },
-});
+  });
 
+  // Menambahkan route ke server
   server.route(routes);
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+
+  // Menggunakan https untuk server
+  const httpsServer = https.createServer(options, server.listener);
+
+  // Menjalankan server
+  await new Promise((resolve, reject) => {
+    httpsServer.listen(5000, (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  });
+
+  console.log(`Server berjalan pada https://${server.info.host}:${server.info.port}`);
 };
 
-
+// Memanggil fungsi init untuk menjalankan server
 init();
